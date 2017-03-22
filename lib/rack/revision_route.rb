905ce1f8491a77d398478cd78ab6ed8d5027a9d5
labocho <labocho@penguinlab.jp>
2017-03-22 16:56:29 +0900

    fix bugs

--- lib/rack/revision_route.rb
+++ lib/rack/revision_route.rb
@@ -4,7 +4,7 @@ module Rack
   class RevisionRoute
     attr_reader :app, :path, :app_dir, :revision
 
-    def initialize(app, path, app_dir)
+    def initialize(app, app_dir, path)
       @app = app
       @path = path
       @app_dir = app_dir
@@ -12,6 +12,7 @@ module Rack
     end
 
     def call(env)
+      debugger
       if env["REQUEST_METHOD".freeze] == "GET".freeze && env["PATH_INFO".freeze] == path
         body = revision.to_s + "\n"
         [200, {"Content-Type" => "text/plain"}, [body]]
@@ -22,14 +23,14 @@ module Rack
 
     private
     def detect_revision
-      revision_file = File.join(app_dir, "REVISION")
-      git_dir = File.join(app_dir, ".git")
+      revision_file = ::File.join(app_dir, "REVISION")
+      git_dir = ::File.join(app_dir, ".git")
 
       case
-      when File.exist?(revision_file)
-        File.read(revision_file).strip
-      when Dir.exist?(git_dir)
-        Dir.chdir(app_dir) do
+      when ::File.exist?(revision_file)
+        ::File.read(revision_file).strip
+      when ::Dir.exist?(git_dir)
+        ::Dir.chdir(app_dir) do
           `git log --format=%H -1`.strip
         end
       end
--- lib/rack/revision_route/version.rb
+++ lib/rack/revision_route/version.rb
@@ -1,5 +1,5 @@
 module Rack
-  module RevisionRoute
+  class RevisionRoute
     VERSION = "0.1.0"
   end
 end
